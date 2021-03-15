d22387f6c00a6ef05f2194dc48ad16697cbafa2f
Badr Rahmaouy <rahmaouy@gmail.com>
2021-03-15 20:09:26 +0100

    setted dev environment

---
+++ webpack.dev.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            title: 'development',
+            template: './src/views/index.html',
+            filename: './index.html'
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+    ]
+}
