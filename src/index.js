c5e5c867fb1c852d98eb737e9ca7d72762f8b4fa
Badr Rahmaouy <rahmaouy@gmail.com>
2021-03-20 20:08:26 +0100

    Landing page 1.0 Finished landing page

--- src/index.js
+++ src/index.js
@@ -4,11 +4,3 @@ import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 
-import { pressLink } from './js/page'
-
-export { pressLink }
-
-window.addEventListener('load', (e) => {
-    const btn = document.getElementById('project1')
-    btn.addEventListener('click', pressLink)
-})
